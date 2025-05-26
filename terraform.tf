2b8b015081ad9f836dd452dc92b9bfe5c46e4b7d
vaplade <makeshifth2h@gmail.com>
2025-05-26 09:35:21 -0700

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
-
+    organization = "vap-projects"
     workspaces {
       name = "learn-terraform"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
